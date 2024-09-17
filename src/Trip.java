e28acb8af7b3afca1965ec0c997ab7b7261a1f52
Tumanov <tslavich@bk.ru>
2024-09-17 13:57:11 +0300

    -Cosmetic

--- src/Trip.java
+++ src/Trip.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
